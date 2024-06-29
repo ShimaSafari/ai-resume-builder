263fb21e6bdb302ce8463a13bfc121bfe43c4832
ShimaSafari <shimasafarimg@gmail.com>
2024-06-29 20:02:12 +0330

    add resume personal detail

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -14,7 +14,11 @@ const CreateNewResume = (data) => axiosClient.post("/user-resumes", data);
 
 const GetUserResumes = (userEmail) =>
   axiosClient.get("/user-resumes?filters[userEmail][$eq]=" + userEmail);
+
+const UpdateResumeDetail = (id, data) =>
+  axiosClient.put("user-resumes/" + id, data);
 export default {
   CreateNewResume,
   GetUserResumes,
+  UpdateResumeDetail,
 };
--- src/dashboard/resume/components/FormSection.jsx
+++ src/dashboard/resume/components/FormSection.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import PersonalDetail from "./forms/PersonalDetail";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight, LayoutGrid } from "lucide-react";
 
 function FormSection() {
+  const [activeFormIndex, setActiveFormIndex] = useState(2);
+  const [enableNext, setEnableNext] = useState(false);
   return (
-    <div>FormSection</div>
-  )
+    <div>
+      <div className="flex items-center justify-between">
+        <Button variant="outline" size="sm" className="flex gap-2">
+          <LayoutGrid /> Theme
+        </Button>
+        <div className="flex gap-2">
+          {activeFormIndex > 1 && (
+            <Button
+              size="sm"
+              onClick={() => setActiveFormIndex(activeFormIndex - 1)}
+            >
+              <ArrowLeft />
+            </Button>
+          )}
+          <Button
+            disabled={!enableNext}
+            className="flex gap-2"
+            size="sm"
+            onClick={() => setActiveFormIndex(activeFormIndex + 1)}
+          >
+            Next <ArrowRight />
+          </Button>
+        </div>
+      </div>
+      {/* personal detail */}
+      {activeFormIndex == 1 ? (
+        <PersonalDetail enabledNext={(v) => setEnableNext(v)} />
+      ) : null}
+      {/* summery */}
+
+      {/* experince */}
+
+      {/* educational detail */}
+
+      {/* skills */}
+    </div>
+  );
 }
 
-export default FormSection
+export default FormSection;
